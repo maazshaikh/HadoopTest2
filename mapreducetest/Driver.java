56b91d015017eff8a91d9ccf8a72aa718811ea3a
Maaz Shaikh <maaz.shaikh@infobeans.com>
2016-11-18 21:50:29 +0530

    Hadoop test 2

---
+++ mapreducetest/Driver.java
@@ -0,0 +1,54 @@
+package com.info.mapreducetest;
+//@see https://hadoop.apache.org/docs/r2.6.0/api/
+
+/**
+ * import all the required packages
+ */
+import java.io.IOException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+
+//The Driver Class starts here, we can say this is the main() of the program.
+public class Driver {
+    
+    public static void main(String... args) throws IOException, InterruptedException, ClassNotFoundException{
+        //The condition will check if the input file, and the output path directory are given as input or not.
+        if(args.length<2){
+            System.out.println("Usage is [generic option] <input path> <output path>");
+            System.exit(1);
+        }
+        //Will create an instance of the JOB, which will control the execution of program.
+        Job job=new Job();
+        
+        //This will set the Jar by finding where a given class came from
+        job.setJarByClass(Driver.class);
+        //This will set any user specified name for the Job
+        job.setJobName("wordCount");
+        
+        //This will process the input file given and to be used by the Mappers & will also set the array of paths as the list of inputs for the map-reduce job.
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+
+        //Processed input to written to a directory in HDFS, used by Reducers & also set the path of the output directory for the map-reduce job.
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        
+        //Name of the class which extends the Mapper Class or Name of the Mapper, which has the logic and performs Map-only JOB
+        job.setMapperClass(MapperTest.class);
+
+        //Name of the class which extends the reducer Class or Name of the Reducer, which has the logic and performs Reduce-only JOB
+        job.setReducerClass(ReducerTest.class);
+        
+        //Result of the of Mapper and Reducer is a combination of (key, value) pairs. It also set the output key datatype as Text for Reducer.
+        job.setOutputKeyClass(Text.class);
+
+        //Set the output key datatype as Text for Reducer. Also set the output key datatype for Reducer.
+        job.setOutputValueClass(IntWritable.class);
+        
+        //Check the status of the JOB and then exit.
+        System.exit(job.waitForCompletion(true)?0:1);
+        
+    }
+}
